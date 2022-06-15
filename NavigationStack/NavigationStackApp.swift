b99d4eff97d4317ca7abdc68ede066972565ab41
James Ruston <jruston90@gmail.com>
2022-06-15 10:22:30 +0100

    rough router implementation using navigationstack

---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by James Ruston on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
